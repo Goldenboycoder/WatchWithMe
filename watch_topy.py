f8bfc10d81ea6c2d18ae118ed46de99afe603331
Patrick Balian <goldenboycoder@gmail.com>
2020-11-14 11:07:15 +0200

    Converted to OOP

--- watch_topy.py
+++ watch_topy.py
@@ -15,155 +15,174 @@ vUrl=''
 Driver=''
 
 
-def sync(username,address):
-    #sync with client by sending the current time
-    vtime=Driver.execute_script("return document.getElementById('movie_player').getCurrentTime()")
-    print("syncing with ",username)
-    return vtime
+class WatchWithMe:
+
+    def __init__(self,hostip=None,port=None,delay=7):
+        self.hostip=hostip
+        self.port=port
+        self.delay=delay
+
+        self.Protocol={'JOIN':self.join , 'SYNC':self.sync}
 
 
-def join(user,address):
-    #provided new users with the video link
-    print(user," joined")
-    return vUrl
+    def sync(self,Driver,username,address):
+        #sync with client by sending the current time
+        vtime=Driver.execute_script("return document.getElementById('movie_player').getCurrentTime()")
+        print("syncing with ",username)
+        return vtime
 
-Protocol={'JOIN':join , 'SYNC':sync}
 
-def getIp():
+    def join(self,user,address):
+        #provided new users with the video link
+        print(user," joined")
+        return vUrl
+
+    #Protocol={'JOIN':join , 'SYNC':sync}
+
+    def getIp(self):
+            try:
+                s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+                internetHost="www.google.com"
+                internetHostport=80
+                s.connect( ( internetHost, internetHostport ) )
+                ip = s.getsockname()[0]
+                s.close()
+                print(ip)
+                return ip
+            except :
+                return False
+
+    def handleConnection(self,Driver,connection,address):
+        #handles client coonections and respond to requests according to the Protocol
+        with connection:
+            print('Connected by {}'.format(address))
+            while True:
+                re = connection.recv(1024)
+                req=re.decode('utf-8')
+                code , data = req.split('-',1)
+                response=self.Protocol[code.upper()](Driver,data,address)
+                connection.sendall(str(response).encode('utf-8'))
+
+    def openYT(self,Driver,url):
+        #open url
+        Driver.get(url)
+
+    def syncYT(self,Driver,htime):
+        #sync client current time with the host
+        plus='+='
+        mminus='-='
+        seek=''
+        ctime=Driver.execute_script("return document.getElementById('movie_player').getCurrentTime()")
+        #calculate time difference 
+        res= float(htime)-float(ctime)
+        if res > 0:
+            seek=plus+str(abs(res))
+        else:
+            seek=mminus+str(abs(res))
+        
+        Driver.execute_script('document.getElementsByTagName("video")[0].currentTime'+seek)
+
+    print("Welcome to watch together")
+    def browserSetup(self,Driver):
+        bType=input("Are you using Chrome or Firefox (selenium drivers  needed) c/f : ")
         try:
-            s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-            internetHost="www.google.com"
-            internetHostport=80
-            s.connect( ( internetHost, internetHostport ) )
-            ip = s.getsockname()[0]
-            s.close()
-            print(ip)
-            return ip
-        except :
+            if bType == 'f':
+                filename=Path("./paths.txt")
+                FirefoxPath=''
+                GeckoDriverPath=''
+                if filename.exists():
+                    file = open(filename,'r') 
+                    paths=file.readlines() 
+                    file.close()
+                    FirefoxPath , GeckoDriverPath= paths
+                    FirefoxPath=FirefoxPath.rstrip()
+                    GeckoDriverPath=GeckoDriverPath.rstrip()
+                else:
+                    print("please provide the paths for firefox.exe and geckodriver.exe respectively using \ \ ")
+                    FirefoxPath=input("firefox.exe path : ")
+                    GeckoDriverPath=input("geckodriver.exe path")
+                    file = open(filename,'w')
+                    file.writelines([FirefoxPath+" \n",GeckoDriverPath+" \n"])
+                    file.close()
+
+                binary=FirefoxBinary(FirefoxPath)
+                Driver=webdriver.Firefox(firefox_binary=binary, executable_path=GeckoDriverPath)
+                return Driver
+            elif bType=='c':
+                filename=Path("./Cpaths.txt")
+                ChromeDriver=''
+                if filename.exists():
+                    file = open(filename,'r') 
+                    paths=file.readline() 
+                    file.close()
+                    ChromeDriver= paths
+                    ChromeDriver=ChromeDriver.rstrip()
+                else:
+                    print("please provide the paths for ChromeDriver.exe using \ \ ")
+                    ChromeDriver=input("ChromeDriver.exe path : ")
+                    file = open(filename,'w')
+                    file.writeline(ChromeDriver+" \n")
+                    file.close()
+                Driver=webdriver.Chrome(ChromeDriver)
+                return Driver
+            else:
+                print('???')
+                return False
+        except Exception as e:
+            print(e)
             return False
 
-def handleConnection(connection,address):
-    #handles client coonections and respond to requests according to the Protocol
-    with connection:
-        print('Connected by {}'.format(address))
-        while True:
-            re = connection.recv(1024)
-            req=re.decode('utf-8')
-            code , data = req.split('-',1)
-            response=Protocol[code.upper()](data,address)
-            connection.sendall(str(response).encode('utf-8'))
 
-def openYT(Driver,url):
-    #open url
-    Driver.get(url)
+    def roleInit(self,Driver,hostip,port):
+        
+        uType=input("Are you the host y/n : ")
 
-def syncYT(Driver,htime):
-    #sync client current time with the host
-    plus='+='
-    mminus='-='
-    seek=''
-    ctime=Driver.execute_script("return document.getElementById('movie_player').getCurrentTime()")
-    #calculate time difference 
-    res= float(htime)-float(ctime)
-    if res > 0:
-        seek=plus+str(abs(res))
-    else:
-        seek=mminus+str(abs(res))
-    
-    Driver.execute_script('document.getElementsByTagName("video")[0].currentTime'+seek)
+        if uType == 'y' :
+            ip=False
+            while not ip :
+                ip=self.getIp()
+                if ip:
+                    hostip=ip
 
-print("Welcome to watch together")
-bType=input("Are you using Chrome or Firefox (selenium drivers  needed) c/f : ")
-
-
-if bType == 'f':
-    filename=Path("./paths.txt")
-    FirefoxPath=''
-    GeckoDriverPath=''
-    if filename.exists():
-        file = open(filename,'r') 
-        paths=file.readlines() 
-        file.close()
-        FirefoxPath , GeckoDriverPath= paths
-        FirefoxPath=FirefoxPath.rstrip()
-        GeckoDriverPath=GeckoDriverPath.rstrip()
-    else:
-        print("please provide the paths for firefox.exe and geckodriver.exe respectively using \ \ ")
-        FirefoxPath=input("firefox.exe path : ")
-        GeckoDriverPath=input("geckodriver.exe path")
-        file = open(filename,'w')
-        file.writelines([FirefoxPath+" \n",GeckoDriverPath+" \n"])
-        file.close()
-
-    binary=FirefoxBinary(FirefoxPath)
-    Driver=webdriver.Firefox(firefox_binary=binary, executable_path=GeckoDriverPath)
-elif bType=='c':
-    filename=Path("./Cpaths.txt")
-    ChromeDriver=''
-    if filename.exists():
-        file = open(filename,'r') 
-        paths=file.readline() 
-        file.close()
-        ChromeDriver= paths
-        ChromeDriver=ChromeDriver.rstrip()
-    else:
-        print("please provide the paths for ChromeDriver.exe using \ \ ")
-        ChromeDriver=input("ChromeDriver.exe path : ")
-        file = open(filename,'w')
-        file.writeline(ChromeDriver+" \n")
-        file.close()
-    Driver=webdriver.Chrome(ChromeDriver)
-else:
-    print('???')
-
-uType=input("Are you the host y/n : ")
-
-if uType == 'y' :
-    ip=False
-    while not ip :
-        ip=getIp()
-        if ip:
-            hostip=ip
-
-    vUrl=input("Enter the url of the youtube video : ")
-    openYT(Driver,vUrl)
-    print(Driver.execute_script("return document.getElementById('movie_player').getCurrentTime()"))
-    try:
-        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-            s.bind((hostip,port))
-            s.listen()
-            print("listening on port : ",port)
-            while True:
-                connection , address = s.accept()
-                thred=threading.Thread(target=handleConnection,args=[connection , address],daemon=True)
-                thred.start()
-                print(Driver.execute_script("return document.getElementById('movie_player').getCurrentTime()"))
-    except Exception as e:
-        print(e)
-else:
-    hostip=input("enter the hosts ip : ")
-    port=int(input("Enter hosts port : "))
-    username=input("Enter username : ")
-    try:
-        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-            s.connect((hostip,port))
-            joined=False
-            while True:
-                if not joined :
-                    #send a join request , response should be a url
-                    req='{}-{}'.format("join",username)
-                    s.sendall(req.encode('utf-8'))
-                    re = s.recv(1024).decode('utf-8')
-                    openYT(Driver,re)
-                    joined=True
-                else:
-                    #send request to sync with host , response should be host current time
-                    time.sleep(7) #interval between each sync
-                    req='{}-{}'.format("sync",username)
-                    s.sendall(req.encode('utf-8'))
-                    re = s.recv(1024)
-                    response = re.decode('utf-8')
-                    syncYT(Driver,response)
-    except Exception as e:
-        print(e)
+            vUrl=input("Enter the url of the youtube video : ")
+            self.openYT(Driver,vUrl)
+            print(Driver.execute_script("return document.getElementById('movie_player').getCurrentTime()"))
+            try:
+                with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+                    s.bind((hostip,port))
+                    s.listen()
+                    print("listening on port : ",port)
+                    while True:
+                        connection , address = s.accept()
+                        thred=threading.Thread(target=self.handleConnection,args=[Driver, connection , address],daemon=True)
+                        thred.start()
+                        print(Driver.execute_script("return document.getElementById('movie_player').getCurrentTime()"))
+            except Exception as e:
+                print(e)
+        else:
+            hostip=input("enter the hosts ip : ")
+            port=int(input("Enter hosts port : "))
+            username=input("Enter username : ")
+            try:
+                with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+                    s.connect((hostip,port))
+                    joined=False
+                    while True:
+                        if not joined :
+                            #send a join request , response should be a url
+                            req='{}-{}'.format("join",username)
+                            s.sendall(req.encode('utf-8'))
+                            re = s.recv(1024).decode('utf-8')
+                            self.openYT(Driver,re)
+                            joined=True
+                        else:
+                            #send request to sync with host , response should be host current time
+                            time.sleep(7) #interval between each sync
+                            req='{}-{}'.format("sync",username)
+                            s.sendall(req.encode('utf-8'))
+                            re = s.recv(1024)
+                            response = re.decode('utf-8')
+                            self.syncYT(Driver,response)
+            except Exception as e:
+                print(e)
 
